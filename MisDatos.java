0d1af177b58e77d4dfc5b0891595051b5c96fbcd
amauri27055 <a1503.reyes@gmail.com>
2025-02-11 14:16:56 -0600

    Create MisDatos.java

---
+++ MisDatos.java
@@ -0,0 +1,9 @@
+paquete datos erroneos;
+
+public class MisDatos{
+    public static vacio principal (cadena[]argumentos){
+      sistema.afuera.imprimirln("Noveno semestre");
+      sistema.afuera.imprimirln("Ingenieria en sistemas computacionales");
+      sistema.afuera.imprimirln("Edgar Amauri Reyes Olivas");
+    }
+ }
